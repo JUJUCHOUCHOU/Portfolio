ef13b49f73a952896c46f37d969cd87e969e7584
Julie <cohen.ju@laposte.net>
2023-09-21 17:13:29 +0200

    control warning

--- src/Pages/SkillsPage/SkillsPage.js
+++ src/Pages/SkillsPage/SkillsPage.js
@@ -1,25 +1,25 @@
-import Navbar from '../../Components/Navbar/Navbar';
-import Footer from '../../Components/Footer/Footer';
-import Data from '../../Data/Data';
-import'./style.scss';
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
+import Data from "../../Data/Data";
+import "./style.scss";
 
-function SkillsPage(){
-  return(
+function SkillsPage() {
+  return (
     <div>
-      <Navbar/>
-      <div className='skillsContent'>
-        {Data.map((item)=>(
-          <div className='container'>
-            <div className='icon'>{item.icon}</div>
-          <div className='containerText'>  
-          <h3>{item.title}</h3>
-          <p>{item.content}</p>
-          </div>
+      <Navbar />
+      <div className="skillsContent">
+        {Data.map((item, index) => (
+          <div key={index} className="container">
+            <div className="icon">{item.icon}</div>
+            <div className="containerText">
+              <h3>{item.title}</h3>
+              <p>{item.content}</p>
+            </div>
           </div>
         ))}
       </div>
-      <Footer/>
-      </div>
-  )
+      <Footer />
+    </div>
+  );
 }
-export default SkillsPage
+export default SkillsPage;
